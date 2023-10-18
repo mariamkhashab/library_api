cfecc5f16c38b72620c820e5daee92cd4a802cba
mariam.elkhashab <mariam.elkhashab@blnk.ai>
2023-10-19 00:25:03 +0300

    setup initial server and configure database

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+app.use(express.json()); // middleware to parse json from request bodies
+require("dotenv").config();
+const server_port = process.env.SERVER_PORT;
+const host = process.env.HOST;
+const user = process.env.USER;
+const db_port = process.env.DB_PORT;
+const db_password = process.env.PASSWORD;
+const db_username = process.env.DATABASE;
+
+const PG_client = require("pg").Client;
+const db_client = new PG_client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "postgres",
+  database: "postgres",
+});
+db_client.connect();
+
+app.get("/", (req, res1) => {
+  db_client.query(`select * from book`, (err, res2) => {
+    if (!err) {
+      res1.status(200).send(res2.rows);
+    } else {
+      console.log(err.message);
+    }
+  });
+});
+
+app.listen(server_port, () => {
+  console.log(`listening on port ${server_port}`);
+});
